b1902327c72dcde2cb3eeacc16dede10a5581e20
AayamKhatiwada <aayamkhatiwada123@gmail.com>
2024-01-14 20:21:36 +0545

    Reolved conflict

--- next.config.js
+++ next.config.js
@@ -1,5 +1,4 @@
 /** @type {import('next').NextConfig} */
-<<<<<<< HEAD
 const nextConfig = {
     images:{
       domains:["lh3.googleusercontent.com","firebasestorage.googleapis.com"]
@@ -7,8 +6,3 @@ const nextConfig = {
   }
   
   module.exports = nextConfig
-=======
-const nextConfig = {}
-
-module.exports = nextConfig
->>>>>>> 6b4599e5688a37e9bfbd18cf2c00135b0f1fd3d2
